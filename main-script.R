66509fea999654121576bfd49507379052973dca
AimanxxAnsari <aimanansari798@gmail.com>
2024-06-15 19:32:58 +0100

    adding r files

---
+++ main-script.R
@@ -0,0 +1,137 @@
+library(readr)
+library(ggplot2)
+library(gridExtra)
+library(fpp)
+library(fpp2)
+library(fma)
+
+df <- read.csv("/Users/biryani/Documents/ts-project/avocado.csv")
+
+# Split the data into training and test datasets
+test_data <- subset(df, Date >= as.Date("2018-01-01") & Date <= as.Date("2018-03-11"))
+training_data <- subset(df, Date <= as.Date("2017-12-31"))
+
+## b
+summary(training_data)
+training_data_clean <- na.omit(training_data)
+
+eda_df <- training_data_clean #duplicating df for exploratory data analysis part
+
+#setting the month, month_year and year
+eda_df$month_year <- format(as.Date(training_data_clean$Date), "%Y-%m")
+eda_df$month <- format(as.Date(training_data_clean$Date), "%m")
+eda_df$year <- format(as.Date(training_data_clean$Date), "%Y")
+
+eda_df$monthabb <- sapply(eda_df$month, function(x) month.abb[as.numeric(x)])
+eda_df$monthabb = factor(eda_df$monthabb, levels = month.abb)
+
+conv_eda <- eda_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "conventional") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) + geom_point(color = "#F35D5D", size = 2) + geom_line(color = "#7FB3D5", group = 1) + 
+  labs(title = "Conventional Avocados", x = "Month", y = "Average Price") + theme_minimal() 
+
+# For Organic Avocados
+org_eda <- eda_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "organic") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) + geom_point(color = "#F35D5D", size = 2) + geom_line(color = "#7FB3D5", group = 1) +
+  labs(title = "Organic Avocados", x = "Month", y = "Average Price") + theme_minimal()  
+
+#creating seperate df for each type
+org_df <- filter(eda_df, type == "organic")
+conv_df <- filter(eda_df, type == "conventional")
+
+org_df$Date <- as.Date(org_df$Date)
+conv_df$Date <- as.Date(conv_df$Date)
+
+#plotting the conv and org plot for average price
+conv_plot <- conv_df %>%
+  ggplot(aes(x=Date, y=AveragePrice)) + geom_line(color="#7FB3D5") + theme_minimal() + 
+  theme(plot.title=element_text(hjust=0.5)) + 
+  labs(title="Conventional Avocados")
+
+org_plot <- org_df %>%
+  ggplot(aes(x=Date, y=AveragePrice)) + geom_line(color="#7FB3D5") + theme_minimal() + 
+  theme(plot.title=element_text(hjust=0.5)) + 
+  labs(title="Organic Avocados")
+
+grid.arrange(org_plot, org_eda, conv_plot, conv_eda, ncol = 2)
+
+#modelling and forecasting of the dataset
+
+conv_ts <- ts(conv_df$AveragePrice, start=c(2015, 1), frequency=52, end = c(2017,52))
+org_ts <- ts(org_df$AveragePrice, start=c(2015, 1), frequency=52, end = c(2017,52))
+
+#conventional arima modelling
+arima_conv <- auto.arima(conv_ts, d=1, D=1, stepwise=FALSE, approximation=FALSE, trace=TRUE)
+
+print(summary(arima_conv))
+
+#organic arima modelling
+arima_os <- auto.arima(org_ts, d=1, D=1, stepwise=FALSE, approximation=FALSE, trace=TRUE)
+
+#forecasting of both types
+forecast_os <- forecast(arima_os, h = 10)
+forecast_cv <- forecast(arima_conv, h = 10)
+
+plot_os <- autoplot(forecast_os)
+plot_cv <- autoplot(forecast_cv)
+
+grid.arrange(plot_os, plot_cv, nrow = 2) #plotting the forecast graph
+
+#residual analysis for conventional avocados
+residuals_cv <- residuals(arima_conv)
+resid_cv_ts <- ts(residuals_cv, start=c(2015, 1), frequency=52)
+
+#residual analysis for organic avocados
+
+residuals_os <- residuals(arima_os)
+resid_os_ts <- ts(residuals_os, start=c(2015, 1), frequency=52)
+
+plot.new()
+#plotting residual, acf, pacf
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+
+plot(resid_cv_ts, main="Residuals", ylab="", xlab="Year")
+acf(resid_cv_ts, main="ACF", ylim = c(-0.2, 0.2))
+pacf(resid_cv_ts, main="PACF")
+
+#plotting residual, acf, pacf
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+
+plot(resid_os_ts, main="Residuals", ylab="", xlab="Year")
+acf(resid_os_ts, main="ACF", ylim = c(-0.2, 0.2))
+pacf(resid_os_ts, main="PACF")
+
+# Evaluating the model
+# test data to time series
+test_data_ts <- ts(na.omit(test_data)$AveragePrice, start=c(2018, 1), frequency=52, end = c(2018, 10))
+
+# assigning mean
+forecast_values_os = forecast_os$mean
+forecast_values_cv = forecast_cv$mean
+
+mse_value_os <- mean((as.vector(test_data_ts) - forecast_values_os)^2)
+mse_value_cv <- mean((as.vector(test_data_ts) - forecast_values_cv)^2)
+
+#print(mse_value_cv)
+#print(mse_value_os)
+
+
+mse_cv <- mse_value_cv
+mse_os <- mse_value_os
+
+# Creating a data frame for the table
+mse_df <- data.frame(Type = c("CV", "OS"),
+                     MSE = c(mse_cv, mse_os))
+knitr::kable(mse_df)
+
+
+
+
+
---
+++ main.R
@@ -0,0 +1,224 @@
+library(readr)
+data1 <- read_csv("/Users/biryani/Documents/ts-project/avocado.csv")
+
+#head(data1)
+
+# Assuming your data frame is named 'avocado_data'
+# Convert the 'Date' column to a Date object
+data1$Date <- as.Date(data1$Date)
+
+# Split the data into training and test datasets
+test_data <- subset(data1, Date >= as.Date("2018-01-01") & Date <= as.Date("2018-03-11"))
+training_data <- subset(data1, Date <= as.Date("2017-12-31"))
+
+# Verify the dimensions of the datasets
+dim(training_data)
+dim(test_data)
+
+library(ggplot2)
+
+ggplot(data1, aes(x = AveragePrice)) +
+  geom_histogram(fill = "skyblue", color = "black") +
+  labs(title = "Distribution of Average Price")
+
+ggplot(data1, aes(x = type, y = AveragePrice, fill = type)) +
+  geom_boxplot() +
+  labs(title = "Average Price Distribution by Type")
+
+ggplot(data1, aes(x = Total.Volume, y = AveragePrice)) +
+  geom_point() +
+  labs(title = "Scatterplot of Average Price vs. Total Volume")
+
+correlation_matrix <- cor(data1[, c("Total.Volume", "AveragePrice", "X4046", "X4225", "X4770")])
+correlation_matrix
+
+#summary and the blank correlation matrix describes the presence of NA values in AveragePrice
+data_clean <- na.omit(data1)
+
+summary(data_clean)
+
+correlation_matrix <- cor(data_clean[, c("Total.Volume", "AveragePrice", "X4046", "X4225", "X4770")])
+correlation_matrix
+
+options(repr.plot.width=8, repr.plot.height=4)
+ggplot(data_clean, aes(x=AveragePrice, fill=type)) + geom_density() + facet_wrap(~type) + theme_minimal() + 
+  theme(plot.title=element_text(hjust=0.5), legend.position="bottom") + labs(title="Avocado Price by Type") + scale_fill_brewer(palette="Set1")
+
+#seasonal chart
+seasonal_df <- data_clean
+
+seasonal_df$month_year <- format(as.Date(data_clean$Date), "%Y-%m")
+seasonal_df$month <- format(as.Date(data_clean$Date), "%m")
+seasonal_df$year <- format(as.Date(data_clean$Date), "%Y")
+
+
+seasonal_df$monthabb <- sapply(seasonal_df$month, function(x) month.abb[as.numeric(x)])
+seasonal_df$monthabb = factor(seasonal_df$monthabb, levels = month.abb)
+
+
+# # Let's see if there are seasonal patterns with conventional avocadoes
+ggplot(seasonal_df, aes(x = AveragePrice, fill = as.factor(year))) + 
+  geom_density(alpha = .5) +
+  facet_wrap(~ year) + theme(plot.title=element_text(hjust=0.5), plot.background=element_rect(fill="#F9E79F")) + 
+  guides(fill = FALSE) + labs(title="Distribution of Prices by year", x = 'Average Price', y = 'Density') + 
+  scale_fill_manual(values=c("#2E64FE", "#40FF00", "#FE642E", "#FE2E2E"))
+
+
+library(dplyr)
+# Detecting seasonality patterns
+conv_patterns <- seasonal_df %>% select(monthabb, AveragePrice, type) %>% filter(type == "conventional") %>%
+  group_by(monthabb) %>% summarize(avg=mean(AveragePrice)) %>%
+  ggplot(aes(x=monthabb, y=avg)) + geom_point(color="#F35D5D", aes(size=avg)) + geom_line(group=1, color="#7FB3D5") + 
+  theme(legend.position="none", plot.title=element_text(hjust=0.5), plot.background=element_rect(fill="#F9E79F")) + 
+  labs(title="Conventional Avocados", x="Month", y="Average Price")
+
+
+org_patterns <- seasonal_df %>% select(monthabb, AveragePrice, type) %>% filter(type == "organic") %>%
+  group_by(monthabb) %>% summarize(avg=mean(AveragePrice)) %>%
+  ggplot(aes(x=monthabb, y=avg)) + geom_point(color="#F35D5D", aes(size=avg)) + geom_line(group=1, color="#58D68D") + 
+  theme(legend.position="none", plot.title=element_text(hjust=0.5), plot.background=element_rect(fill="#F9E79F")) + 
+  labs(title="Organic Avocados", x="Month", y="Average Price")
+
+plot(org_patterns)
+plot(conv_patterns)
+
+
+conv_pattern <- seasonal_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "conventional") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) +
+  geom_point(color = "#F35D5D", size = 3) +  # Adding points
+  geom_line(color = "#7FB3D5", group = 1) +   # Adding line
+  labs(title = "Conventional Avocados", x = "Month", y = "Average Price") +
+  theme_minimal()  # Remove any background or foreground specifications
+
+# For Organic Avocados
+org_pattern <- seasonal_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "organic") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) +
+  geom_point(color = "#F35D5D", size = 3) +  # Adding points
+  geom_line(color = "#58D68D", group = 1) +   # Adding line
+  labs(title = "Organic Avocados", x = "Month", y = "Average Price") +
+  theme_minimal()  # Remove any background or foreground specifications
+
+plot(org_pattern)
+plot(conv_pattern)
+
+                ## new day new vibes ## 
+
+summary(training_data)
+training_data_clean <- na.omit(training_data)
+
+seasonal_df <- training_data_clean
+
+seasonal_df$month_year <- format(as.Date(training_data_clean$Date), "%Y-%m")
+seasonal_df$month <- format(as.Date(training_data_clean$Date), "%m")
+seasonal_df$year <- format(as.Date(training_data_clean$Date), "%Y")
+
+
+seasonal_df$monthabb <- sapply(seasonal_df$month, function(x) month.abb[as.numeric(x)])
+seasonal_df$monthabb = factor(seasonal_df$monthabb, levels = month.abb)
+
+conv_pattern1 <- seasonal_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "conventional") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) +
+  geom_point(color = "#F35D5D", size = 3) +  # Adding points
+  geom_line(color = "#7FB3D5", group = 1) +   # Adding line
+  labs(title = "Conventional Avocados TDC", x = "Month", y = "Average Price") +
+  theme_minimal()  # Remove any background or foreground specifications
+
+# For Organic Avocados
+org_pattern1 <- seasonal_df %>%
+  select(monthabb, AveragePrice, type) %>%
+  filter(type == "organic") %>%
+  group_by(monthabb) %>%
+  summarize(avg = mean(AveragePrice)) %>%
+  ggplot(aes(x = monthabb, y = avg)) +
+  geom_point(color = "#F35D5D", size = 3) +  # Adding points
+  geom_line(color = "#58D68D", group = 1) +   # Adding line
+  labs(title = "Organic Avocados TDC", x = "Month", y = "Average Price") +
+  theme_minimal()  # Remove any background or foreground specifications
+
+plot(org_pattern1)
+plot(conv_pattern1)
+
+          ##    fitting a statistical model     ##
+
+head(training_data_clean)
+library(fpp)
+library(fpp2)
+library(fma)
+training_data_clean$Date <- as.Date(training_data_clean$Date, format = "%Y-%m-%d")
+
+training_data_clean_df$Date <- as.Date(training_data_clean_df$Date)
+
+# Convert data frame to time series object
+training_data_ts <- ts(training_data_clean_df$AveragePrice, frequency = 1, start = 1)
+
+# Now try using autoplot with the time series object
+autoplot(training_data_ts)
+
+model1.1 <- auto.arima(training_data_ts)
+model1.1
+
+plot(forecast(model1.1))
+
+arima_model <- auto.arima(training_data_ts, d=1, D=1, stepwise=FALSE, approximation=FALSE, trace=TRUE)
+
+print(summary(arima_model))
+checkresiduals(arima_model) + theme_minimal() + scale_x_date(date_breaks = "1 year", date_labels = "%X")
+
+arima_forecast <- forecast(arima_model)
+
+#autoplot(arima_forecast, include = 70) + theme_minimal() + theme(plot.title = element_text(hjust=0.5), plot.background = element_rect(fill = "#F4F6F7"), legend.position = "bottom", legend.background = element_rect(fill = "#FFF9F5", size = 0.5, linetype = "solid", color = "blank")) +
+ # labs(title = "Forecasting", x = "Date", y = "Price") 
+class(arima_forecast$Date)
+
+# If the class is not Date, convert it to Date class
+if (!inherits(arima_forecast$Date, "Date")) {
+  arima_forecast$Date <- as.Date(arima_forecast$Date)
+}
+
+# Get the minimum and maximum dates
+min_date <- min(arima_forecast$Date)
+max_date <- max(arima_forecast$Date)
+
+# Plot with the converted Date variable and set x-axis limits
+autoplot(arima_forecast, include = 70, x = Date) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.background = element_rect(fill = "#F4F6F7"),
+        legend.position = "bottom",
+        legend.background = element_rect(fill = "#FFF9F5", size = 0.5, linetype = "solid", color = "blank")) +
+  labs(title = "Forecasting", x = "Date", y = "Price") +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")
+
+
+
+library(fpp)
+library(fpp2)
+library(fma)
+tdcx <- training_data_clean
+
+tdcx$Date <- as.Date(tdcx$Date, format = "%Y-%m-%d")
+tdcx_ts <- ts(tdcx$Date, frequency = 1, start = 1)
+
+autoplot(tdcx_ts)
+
+arima_model <- auto.arima(training_data_ts, d=1, D=1, stepwise=FALSE, approximation=FALSE, trace=TRUE)
+
+print(summary(arima_model))
+checkresiduals(arima_model, x = Date) + theme_minimal()
+
+arima_forecast <- forecast(arimaarima_forecast <- forecast(arima_model)
+                           
+plot(arima_forecast)
+                           
